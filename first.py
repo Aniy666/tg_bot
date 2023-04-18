87e3f5306b37c35c12e00bc115d5cbdcec1b0f1e
Anna Dyachenko <Aniy666@mail.ru>
2023-04-18 14:56:13 +0300

    Задание Угадай число

--- first.py
+++ first.py
@@ -1,3 +1,21 @@
-print('This is a file from Github repository')
-
-print('These are new local changes')
+from random import *
+n = randint(1, 100)
+print(n)
+print('Добро пожаловать в числовую угадайку')
+def is_valid(text):
+    return text.isdigit() and 100>= int(text) >= 1
+again = 0
+count = 0
+while again != n:
+    print('Введите число от 1 до 100')
+    again = input()
+    while not is_valid(again):
+        print('А может быть все-таки введем целое число от 1 до 100?')
+        again = input()
+    count += 1
+    again = int(again)
+    if again > n: print('Ваше число больше загаданного, попробуйте еще разок')
+    elif again < n: print('Ваше число меньше загаданного, попробуйте еще разок')
+    else: print('Вы угадали, поздравляем!')
+print('Спасибо, что играли в числовую угадайку. Еще увидимся...')
+print('Количество попыток:', count)
